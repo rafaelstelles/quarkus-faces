7364e9d31329d6e8b147b87b4750450d825c63a6
Rafael Telles <rafael@bzion.com.br>
2023-11-22 08:43:24 -0400

    Adding example for rewrite

---
+++ src/main/java/org/primefaces/showcase/rewrite/RewriteView.java
@@ -0,0 +1,11 @@
+package org.primefaces.showcase.rewrite;
+
+import java.io.Serializable;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+@Named
+@ViewScoped
+public class RewriteView implements Serializable {
+}
